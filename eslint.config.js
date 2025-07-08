963145f6055613067e9858f119293d2d0d6c48a8
naveenkp-tv <naveenkp@testvagrant.com>
2025-07-08 12:37:02 +0530

    Add ESLint configuration file to enforce coding standards and improve code quality

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {
+      // Add your custom rules here
+    },
+  },
+];
